cde56ac7b70ecd35c511cd0ccb41e4783a772d14
wexmaster <70749863+wexmaster@users.noreply.github.com>
2025-09-11 11:57:56 +0200

    Update Config

--- config.go
+++ config.go
@@ -3,6 +3,15 @@ package opentelemetryexportermonitoring
 import "time"
 
 type Config struct {
-	Endpoint string        `mapstructure:"endpoint"`
-	Timeout  time.Duration `mapstructure:"timeout"`
+	// Opción global (opcional). Si la pones y no defines los específicos,
+	// se usará <Endpoint>/v1/{traces|metrics|logs}.
+	Endpoint string `mapstructure:"endpoint"`
+
+	// Endpoints completos por señal (recomendado).
+	TracesEndpoint  string `mapstructure:"traces_endpoint"`
+	MetricsEndpoint string `mapstructure:"metrics_endpoint"`
+	LogsEndpoint    string `mapstructure:"logs_endpoint"`
+
+	Timeout time.Duration     `mapstructure:"timeout"`
+	Headers map[string]string `mapstructure:"headers"`
 }
